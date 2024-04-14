c1c7241a501de8c03372895c42df5824fce46963
Pascal Kleindienst <pascal.kleindienst@posteo.de>
2024-04-14 14:44:23 +0200

    add application mark

--- resources/js/Components/ApplicationMark.vue
+++ resources/js/Components/ApplicationMark.vue
@@ -1,6 +1,79 @@
 <template>
-    <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z" fill="#6875F5" />
-        <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5" />
+    <svg
+        id="euF8xR3BPc51"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 200 75"
+        shape-rendering="geometricPrecision"
+        text-rendering="geometricPrecision"
+    >
+        <text
+            dx="0"
+            dy="0"
+            font-family="Audiowide"
+            font-size="48"
+            font-weight="400"
+            transform="translate(71.230876 63.6348)"
+            stroke-width="0"
+            fill="var(--_logoColor, hsl(var(--foreground)))"
+        >
+            <tspan y="0" font-weight="400" stroke-width="0"><![CDATA[udivi]]></tspan>
+        </text>
+        <polygon
+            points="0,-30 25.980762,15 -25.980762,15 0,-30"
+            transform="translate(38.165283 44.585663)"
+            stroke="var(--_logoHighlight, hsl(var(--primary)))"
+            fill="var(--_logoColor, hsl(var(--foreground)))"
+            stroke-width="10"
+        />
+        <rect
+            width="34.141541"
+            height="3.687541"
+            rx="0"
+            ry="0"
+            transform="matrix(1.092473 0 0 1.547811 19.515928 53.393988)"
+            fill="var(--_logoBackground, hsl(var(--background)))"
+            stroke-width="0"
+        />
+        <rect
+            width="34.141541"
+            height="3.687541"
+            rx="0"
+            ry="0"
+            transform="matrix(.588256-.999611 1.333966 0.785019 15.609294 56.206816)"
+            fill="var(--_logoBackground, hsl(var(--background)))"
+            stroke-width="0"
+        />
+        <rect
+            style="isolation: isolate"
+            width="34.141541"
+            height="3.687541"
+            rx="0"
+            ry="0"
+            transform="matrix(-.787478-1.363952 1.340444-.773905 58.319542 63.135009)"
+            fill="var(--_logoBackground, hsl(var(--background)))"
+            fill-rule="evenodd"
+            stroke-width="0"
+        />
+        <path
+            d="M82.52835,114.1016l-3.39639,5.49483h-6.56l5.149578-8.558713L82.52835,114.1016Z"
+            transform="translate(-62.02-55)"
+            fill="var(--_logoBackground, hsl(var(--background)))"
+            stroke="var(--_logoHighlight, hsl(var(--primary)))"
+            stroke-width="0"
+        />
+        <path
+            d="M81.089765,116.317769L79.13196,119.59643h-6.56l5.072112-8.978165l3.445693,5.699504Z"
+            transform="matrix(-.512212 0.858859-.858859-.512212 200.502696 54.574393)"
+            fill="var(--_logoBackground, hsl(var(--background)))"
+            stroke="var(--_logoHighlight, hsl(var(--primary)))"
+            stroke-width="0"
+        />
+        <path
+            d="M81.12671,116.202712l-.998649,1.833584-6.613218.078444l4.145068-7.412658l3.466799,5.50063Z"
+            transform="matrix(-.512212 0.858859-.858859-.512212 173.699815 8.00356)"
+            fill="var(--_logoBackground, hsl(var(--background)))"
+            stroke="var(--_logoHighlight, hsl(var(--primary)))"
+            stroke-width="0"
+        />
     </svg>
 </template>
