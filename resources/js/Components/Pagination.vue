812ac796d72b8fb67f617830ef1ca3c947a57563
Pascal Kleindienst <pascal.kleindienst@posteo.de>
2024-05-03 11:05:01 +0200

    add pagination component

---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import {
+    Pagination,
+    PaginationEllipsis,
+    PaginationFirst,
+    PaginationLast,
+    PaginationList,
+    PaginationListItem,
+    PaginationNext,
+    PaginationPrev
+} from '@/Components/ui/pagination';
+import { Button } from '@/Components/ui/button';
+import { PaginatedDataCollection } from '@/types/inertia';
+
+const props = defineProps<{ paginator: PaginatedDataCollection<unknown>; only?: string[] }>();
+
+const options: { only?: string[] } = {};
+
+if (props.only) {
+    options.only = props.only;
+}
+</script>
+
+<template>
+    <Pagination
+        v-if="paginator.meta.last_page > 1"
+        v-slot="{ page }"
+        :total="paginator.meta.total"
+        :items-per-page="paginator.meta.per_page"
+        :sibling-count="1"
+        show-edges
+        :default-page="paginator.meta.current_page"
+        class="mx-auto mt-8"
+        @update:page="
+            (currentPage) => {
+                let url = paginator.links[currentPage]?.url;
+                if (url) {
+                    $inertia.visit(url, options);
+                }
+            }
+        "
+    >
+        <PaginationList v-slot="{ items }" class="flex items-center gap-1">
+            <PaginationFirst />
+            <PaginationPrev />
+
+            <template v-for="(item, index) in items">
+                <PaginationListItem v-if="item.type === 'page'" :key="index" :value="item.value" as-child>
+                    <Button class="h-10 w-10 p-0" :variant="item.value === page ? 'default' : 'outline'">
+                        {{ item.value }}
+                    </Button>
+                </PaginationListItem>
+                <PaginationEllipsis v-else :key="item.type" :index="index" />
+            </template>
+
+            <PaginationNext />
+            <PaginationLast />
+        </PaginationList>
+    </Pagination>
+</template>
--- resources/js/types/inertia.d.ts
+++ resources/js/types/inertia.d.ts
@@ -11,6 +11,28 @@ export interface User {
     created_at: string;
 }
 
+export interface PaginatedDataCollection<T> {
+    data: T[];
+    links: Array<{
+        url: string | null;
+        label: string;
+        active: boolean;
+    }>;
+    meta: {
+        current_page: number;
+        first_page_url: string;
+        from: number;
+        last_page: number;
+        last_page_url: string;
+        next_page_url: string | null;
+        path: string;
+        per_page: number;
+        prev_page_url: string | null;
+        to: number;
+        total: number;
+    };
+}
+
 export interface SharedProps extends PageProps {
     auth: {
         user: User;
