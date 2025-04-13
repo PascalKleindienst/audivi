863297f2eca23aeabfb42d898ae2696fae8d80aa
Pascal Kleindienst <pascal.kleindienst@posteo.de>
2025-04-13 08:46:29 +0200

    add banner

--- resources/js/Layouts/AppSidebarLayout.vue
+++ resources/js/Layouts/AppSidebarLayout.vue
@@ -4,6 +4,7 @@ import AppShell from '@/Components/App/AppShell.vue';
 import AppSidebar from '@/Components/App/AppSidebar.vue';
 import AppSidebarHeader from '@/Components/App/AppSidebarHeader.vue';
 import type { BreadcrumbItemType } from '@/types';
+import Banner from '@/Components/Banner.vue';
 
 interface Props {
     breadcrumbs?: BreadcrumbItemType[];
@@ -19,6 +20,8 @@ withDefaults(defineProps<Props>(), {
         <AppSidebar />
         <AppContent variant="sidebar">
             <AppSidebarHeader :breadcrumbs="breadcrumbs" />
+
+            <Banner />
             <slot />
         </AppContent>
     </AppShell>
